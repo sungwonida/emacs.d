b78cdfa48f3f8f1723ed2d7d311e88a7a1bf5a04
David Jung <swjung@canvasbio.com>
2018-08-23 10:39:24 +0900

    Update customization

--- local/custom.el
+++ local/custom.el
@@ -4,14 +4,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
+ '(cua-enable-cua-keys nil)
+ '(cua-mode t nil (cua-base))
  '(custom-safe-themes
    (quote
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(display-time-day-and-date t)
+ '(display-time-mode t)
+ '(dynamic-completion-mode t)
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(global-auto-revert-mode t)
  '(indent-tabs-mode nil)
  '(ns-command-modifier (quote meta))
  '(package-selected-packages
    (quote
     (smartparens virtualenvwrapper unicode-fonts tern-auto-complete smart-mode-line rainbow-mode rainbow-delimiters popwin plantuml-mode pallet nyan-mode markdown-mode magit jedi impatient-mode helm-ls-git helm-gtags helm-git-grep helm-flycheck helm-ag google-translate exec-path-from-shell edit-server diminish company color-identifiers-mode cider bind-key anzu)))
+ '(show-paren-mode t)
  '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
