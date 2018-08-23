0754f9021e1ad3cbe11baf82ec50b181f362c783
David Jung <swjung@canvasbio.com>
2018-08-23 13:38:06 +0900

    Update bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,17 @@ if [ -z $cask ]; then
     # If not found, install it
     echo "Installing Cask"
     curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-    echo "" >> ~/.bashrc
-    echo "# Cask" >> ~/.bashrc
-    echo "export PATH=\"$HOME/.cask/bin:\$PATH\"" >> ~/.bashrc
+
+    # Add lines to export PATH if needed
+    env_path=`echo $PATH`
+    if [[ ! $env_path =~ .*\.cask\/bin.* ]]; then
+        echo "" >> ~/.bashrc
+        echo "# Cask" >> ~/.bashrc
+        echo "export PATH=\"$HOME/.cask/bin:\$PATH\"" >> ~/.bashrc
+    fi
+
+    # Cask bootstrapping is done
 fi
-# Cask bootstrapping is done
 
 # Install the packages and their dependancies
 echo "Installing packages & dependancies"
