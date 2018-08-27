7ce23f76475aee9b6e015c38b8b515f326b4160c
David Jung <swjung@canvasbio.com>
2018-08-27 14:06:01 +0900

    Update c-default-style

--- local/custom.el
+++ local/custom.el
@@ -4,6 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
+ '(c-default-style
+   (quote
+    ((c-mode . "k&r")
+     (c++-mode . "k&r")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
  '(cua-enable-cua-keys nil)
  '(cua-mode t nil (cua-base))
  '(custom-safe-themes
