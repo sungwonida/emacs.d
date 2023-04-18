5b2a48771d50ef80920cebb5093152b85575dd44
Sungwon Jung <sungwon.jung@stradvision.com>
2023-04-18 23:04:39 +0900

    Remove obsolete files

--- local/packages/redo+-20131117.351/redo+-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; redo+-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "redo+" "redo+.el" (0 0 0 0))
-;;; Generated autoloads from redo+.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "redo+" '("undo" "redo" "last-buffer-undo-list")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; redo+-autoloads.el ends here
