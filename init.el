45ffa3f0a21ac31516080abc015f0de9a47cdbcf
David Jung <sungwonida@gmail.com>
2024-07-01 10:20:30 +0900

    Change the theme to a more readable one (doom-one)

--- init.el
+++ init.el
@@ -132,11 +132,23 @@
 (setq org-default-notes-file
       (expand-file-name "note.org" org-root-path))
 
-;; theme
-(use-package vscode-dark-plus-theme
+(use-package doom-themes
+  :ensure t
   :config
-  (load-theme 'vscode-dark-plus t)
-  (set-mouse-color "white"))
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 ;; face
 ;;  Monaco for Linux
